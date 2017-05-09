a48f4c956b51496350a7d6a9fbebfe92d109ab26
David J Eddy <me@davidjeddy.com>
2017-05-08 21:06:51 -0400

    ADDED init codeception based UNIT and FUNCTIONAL test classes

---
+++ tests/unit/WeatherTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class WeatherTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+    }
+}
