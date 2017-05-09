5c5e8da14b9548510ef2f0c8acec248a9746294b
David J Eddy <me@davidjeddy.com>
2017-05-08 21:10:31 -0400

    MOVED application code to ./src UPDATED .dockerignore

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 // include the composer autoloader
-require_once __DIR__ . '/vendor/autoload.php'; 
+require_once __DIR__ . '/../vendor/autoload.php'; 
 
 // import the Symfony Console Application 
 use Symfony\Component\Console\Application; 
