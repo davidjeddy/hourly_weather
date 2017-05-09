7a4f7c3c175e2ec54a1c2a5d30e1374218e78336
David J Eddy <me@davidjeddy.com>
2017-05-08 20:59:28 -0400

    Working

--- docs/notes.md
+++ docs/notes.md
@@ -1,3 +1,7 @@
 notes:
+
 updated PHP service to install composer and run composer install automatically
+apt-get update -y
+apt-get install -y wget git
+
 `php composer.phar install --ansi --profile --prefer-source -o -vvv`
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php 
-echo "Working\n";
+<?php
+// include the composer autoloader
+require_once __DIR__ . '/vendor/autoload.php'; 
+
+// import the Symfony Console Application 
+use Symfony\Component\Console\Application; 
+
+$app = new Application();
+$app->run();
 ?>
